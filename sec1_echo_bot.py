173c70f5f8666f54dacc6c88d5b8acb116a355eb
Justin <jstrat1.618@gmail.com>
2019-03-22 10:50:32 -0600

    initial commit

---
+++ sec1_echo_bot.py
@@ -0,0 +1,32 @@
+def greeting():
+    print("----------------------------------------")
+    print("         WELCOME")
+    print("I'm Echo Bot")
+    print("I just Echo back whatever you say.")
+    print("----------------------------------------")
+def ask_user():
+    user_input = input("What would you like to say? [Q]uit ")
+
+    return user_input
+
+def echo_respond(msg):
+    bot_output = 'ECHO BOT: I hear you. You said "{}"'.format(msg)
+
+    return bot_output
+
+
+def main():
+    greeting()
+
+    user_input = "dummy string"
+
+    while user_input.upper() != "Q":
+        user_input = ask_user()
+
+        if user_input.upper() != "Q":
+            output = echo_respond(user_input)
+            print(output)
+
+
+if __name__ == '__main__':
+    main()
